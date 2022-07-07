fecf2b7a56e1383c2fd71eb8ed5b0907782d1c69
MaireadOB <108820885+MaireadOB@users.noreply.github.com>
2022-07-07 14:39:10 +0100

    File Upload

---
+++ enerpower-office/app-files/data.js
@@ -0,0 +1,452 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-stairs",
+      "name": "Stairs",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8689262978995203,
+          "pitch": 0.6326717059598472,
+          "rotation": 0,
+          "target": "8-downstairs-hall-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-engineering-office-2",
+      "name": "Engineering Office 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.816418013325217,
+          "pitch": 0.2329589717564673,
+          "rotation": 0,
+          "target": "3-downstairs-hall-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-canteen",
+      "name": "Canteen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.922197769263473,
+          "pitch": 0.33687161639852015,
+          "rotation": 0,
+          "target": "3-downstairs-hall-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-downstairs-hall-1",
+      "name": "Downstairs Hall 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4144606073896915,
+          "pitch": 0.3864957986765134,
+          "rotation": 0,
+          "target": "2-canteen"
+        },
+        {
+          "yaw": 3.007964984056242,
+          "pitch": 0.28850811446843494,
+          "rotation": 0,
+          "target": "1-engineering-office-2"
+        },
+        {
+          "yaw": 2.8367528412930563,
+          "pitch": 0.2431524451703062,
+          "rotation": 0,
+          "target": "8-downstairs-hall-2"
+        },
+        {
+          "yaw": -0.28291076272317817,
+          "pitch": 0.26938706876324403,
+          "rotation": 0,
+          "target": "7-store-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-store-1",
+      "name": "Store 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.894275127779226,
+          "pitch": 0.36130634913345006,
+          "rotation": 0,
+          "target": "5-store-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-store-2",
+      "name": "Store 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.379649209677444,
+          "pitch": 0.2979976670656086,
+          "rotation": 0,
+          "target": "6-store-3"
+        },
+        {
+          "yaw": -1.816124339478133,
+          "pitch": 0.2344967516506351,
+          "rotation": 0,
+          "target": "4-store-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-store-3",
+      "name": "Store 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.095792241427967,
+          "pitch": 0.2590724801511133,
+          "rotation": 0,
+          "target": "7-store-4"
+        },
+        {
+          "yaw": -1.4838773203855418,
+          "pitch": 0.14969986577501437,
+          "rotation": 0,
+          "target": "5-store-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-store-4",
+      "name": "Store 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5533680197901916,
+          "pitch": 0.29100853156826645,
+          "rotation": 0,
+          "target": "3-downstairs-hall-1"
+        },
+        {
+          "yaw": 0.1520375634170481,
+          "pitch": 0.12230446572312204,
+          "rotation": 0,
+          "target": "6-store-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-downstairs-hall-2",
+      "name": "Downstairs Hall 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1179358169867966,
+          "pitch": 0.2622433842061298,
+          "rotation": 0,
+          "target": "9-front-door"
+        },
+        {
+          "yaw": 1.5185924827691446,
+          "pitch": 0.08463577691623136,
+          "rotation": 0,
+          "target": "0-stairs"
+        },
+        {
+          "yaw": 0.012092522502442193,
+          "pitch": 0.2643500615677645,
+          "rotation": 0,
+          "target": "3-downstairs-hall-1"
+        },
+        {
+          "yaw": -0.31961827369245555,
+          "pitch": 0.26034954541048094,
+          "rotation": 0,
+          "target": "1-engineering-office-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-front-door",
+      "name": "Front Door",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.1946713396455273,
+        "pitch": 0.2417623149436121,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.2893019434331485,
+          "pitch": 0.29187477378537885,
+          "rotation": 0,
+          "target": "8-downstairs-hall-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.7550301827025709,
+          "pitch": 0.32269114523054476,
+          "title": "Susan's Office",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Enerpower Office",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
